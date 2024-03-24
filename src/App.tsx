fbc84f892f8643e155fd2251127dd51e40822944
dunlokdev <myloc442@gmail.com>
2024-03-24 21:34:55 +0700

    Sử dụng state cơ bản

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,16 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
 import "./App.css";
-import Test from "./examples/Test";
+// import Test from "./examples/Test";
+import Video10 from "./examples/Video10";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-
-      <Test name={"Dương Mỹ Lộc"} />
+    <div>
+      <Video10 />
     </div>
   );
 }
---
+++ src/examples/Video10.tsx
@@ -0,0 +1,142 @@
+import React, { useState } from "react";
+import { Table } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
+export interface IUser {
+  name: string;
+  age: number | string;
+  city: string;
+}
+
+const local: string[] = [
+  "Hà Nội",
+  "Hồ Chí Minh",
+  "Đà Nẵng",
+  "Hải Phòng",
+  "Cần Thơ",
+  "Biên Hòa",
+  "Nha Trang",
+  "Bắc Ninh",
+  "Vũng Tàu",
+  "Quy Nhơn",
+  "Phan Rang",
+  "Đà Lạt",
+];
+
+const Video10 = () => {
+  const [name, setName] = useState<string>("");
+  const [age, setAge] = useState<number | string>("");
+  const [city, setCity] = useState<string>("");
+
+  const [userList, setUserList] = useState<IUser[] | null>([
+    { name: "Mark", age: 20, city: "Ha Noi" },
+    { name: "Jacob", age: 25, city: "HCM" },
+    { name: "Larry", age: 30, city: "Da Lat" },
+  ]);
+
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const handleAgeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAge(parseInt(e.target.value));
+  };
+
+  const handleCityChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setCity(e.target.value);
+  };
+
+  const resetForm = () => {
+    setName("");
+    setAge(0);
+    setCity("");
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!name || !age || !city) {
+      alert("Vui lòng nhập đủ thông tin");
+      return;
+    }
+
+    setUserList([
+      ...(userList as IUser[]),
+      {
+        name: name,
+        age: age,
+        city: city,
+      },
+    ]);
+    resetForm();
+  };
+
+  return (
+    <div>
+      <div className="d-flex align-items-center justify-content-center">
+        <Form onSubmit={handleSubmit} className="container">
+          <Form.Group className="mb-3">
+            <Form.Label>Họ và tên</Form.Label>
+            <Form.Control
+              type="name"
+              placeholder="Nhập họ tên"
+              value={name}
+              onChange={handleNameChange}
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+            <Form.Label>Tuổi</Form.Label>
+            <Form.Control
+              type="age"
+              placeholder="Nhập tuổi"
+              value={age || ""}
+              onChange={handleAgeChange}
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+            <Form.Select aria-label="" value={city} onChange={handleCityChange}>
+              <option>Chọn thành phố</option>
+              {local.map((city, index) => (
+                <option key={index} value={city}>
+                  {city}
+                </option>
+              ))}
+            </Form.Select>
+          </Form.Group>
+
+          <div className="d-grid gap-2">
+            <Button type="submit">Gửi</Button>
+          </div>
+        </Form>
+      </div>
+
+      <h2 className="mt-5">Kết quả</h2>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Họ và tên</th>
+            <th>Tuổi</th>
+            <th>Thành phố</th>
+          </tr>
+        </thead>
+        <tbody>
+          {userList &&
+            userList.map((user, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{user.name}</td>
+                <td>{user.age}</td>
+                <td>{user.city}</td>
+              </tr>
+            ))}
+        </tbody>
+      </Table>
+    </div>
+  );
+};
+
+export default Video10;
